4eed8ddda08b4e9956924ff42bcfa41fba6180ea
Clément R <zetsyog@gmail.com>
2021-08-25 15:50:07 +0000

    Added domain union support

--- src/gen.c
+++ src/gen.c
@@ -21,7 +21,7 @@ static void atiling_sprint_trahrhe(char *s, atiling_fragment_p);
 static void atiling_gen_iloop(FILE *, atiling_fragment_p, int, osl_strings_p,
 							  int);
 static void gen_macros(FILE *, char *, int);
-static void atiling_gen_header(atiling_fragment_p, atiling_options_p);
+static void atiling_gen_info_file(atiling_fragment_p, atiling_options_p);
 
 /**
  * @brief Generate new code with algebraic tiling and write it in output
@@ -39,13 +39,13 @@ void atiling_gen(FILE *input, FILE *output, atiling_fragment_p fragment,
 	int ignore = ATILING_FALSE;
 	int c;
 
-	if (input == NULL)
+	if (input == NULL) {
 		ATILING_error("null input");
+	}
 
 	rewind(input);
 
 	gen_macros(output, options->output, fragment->id);
-
 	while ((c = fgetc(input)) != EOF) {
 		if (c == '\n') {
 			line++;
@@ -61,7 +61,7 @@ void atiling_gen(FILE *input, FILE *output, atiling_fragment_p fragment,
 		}
 
 		if (ignore == ATILING_FALSE) {
-			fprintf(output, "%c", c);
+			fprintf(output, "%c", (char)c);
 		}
 
 		if (fragment != NULL) {
@@ -71,6 +71,7 @@ void atiling_gen(FILE *input, FILE *output, atiling_fragment_p fragment,
 				ATILING_debug("ending gen");
 			}
 		}
+
 		column++;
 	}
 }
@@ -95,7 +96,7 @@ void atiling_gen_fragment(FILE *output, atiling_fragment_p fragment,
 
 	fprintf(output, "// end atiling\n");
 
-	atiling_gen_header(fragment, options);
+	atiling_gen_info_file(fragment, options);
 
 	osl_strings_free(params);
 }
@@ -255,65 +256,74 @@ static void atiling_gen_cloog_code(FILE *output, osl_scop_p scop, int level) {
 	cloog_state_free(state);
 }
 
-static void atiling_gen_domain(osl_statement_p stmt, osl_relation_p base,
-							   int orig_params, int it) {
-	int precision = base->precision;
+static void atiling_gen_domain(osl_statement_p stmt, int orig_params, int it) {
+	osl_relation_p base = stmt->domain;
+	int precision		= base->precision;
 
-	osl_vector_p vec = osl_vector_pmalloc(precision, base->nb_columns);
+	osl_vector_p vec;
 
-	// i - lbi >= 0
-	osl_int_set_si(precision, &(vec->v[0]), 1);		 // >= 0
-	osl_int_set_si(precision, &(vec->v[1 + it]), 1); // i
-	osl_int_set_si(precision,
-				   &(vec->v[1 + base->nb_output_dims + orig_params + it * 2]),
-				   -1); // -lbi
+	while (base != NULL) {
 
-	osl_relation_insert_vector(base, vec, -1);
-	osl_vector_free(vec);
+		vec = osl_vector_pmalloc(precision, base->nb_columns);
 
-	vec = osl_vector_pmalloc(precision, base->nb_columns);
+		// i - lbi >= 0
+		osl_int_set_si(precision, &(vec->v[0]), 1);		 // >= 0
+		osl_int_set_si(precision, &(vec->v[1 + it]), 1); // i
+		osl_int_set_si(
+			precision,
+			&(vec->v[1 + base->nb_output_dims + orig_params + it * 2]),
+			-1); // -lbi
 
-	// -i + ubi >= 0
-	osl_int_set_si(precision, &(vec->v[0]), 1);		  // >= 0
-	osl_int_set_si(precision, &(vec->v[1 + it]), -1); // -i
-	osl_int_set_si(
-		precision,
-		&(vec->v[1 + base->nb_output_dims + orig_params + it * 2 + 1]),
-		1); // ubi
+		osl_relation_insert_vector(base, vec, -1);
+		osl_vector_free(vec);
 
-	osl_relation_insert_vector(base, vec, -1);
-	osl_vector_free(vec);
+		vec = osl_vector_pmalloc(precision, base->nb_columns);
 
-	vec = osl_vector_pmalloc(precision, base->nb_columns);
+		// -i + ubi >= 0
+		osl_int_set_si(precision, &(vec->v[0]), 1);		  // >= 0
+		osl_int_set_si(precision, &(vec->v[1 + it]), -1); // -i
+		osl_int_set_si(
+			precision,
+			&(vec->v[1 + base->nb_output_dims + orig_params + it * 2 + 1]),
+			1); // ubi
 
-	// ubi >= 0 && ubi -lbi >= 0
-	osl_int_set_si(precision, &(vec->v[0]), 1); // >= 0
-	osl_int_set_si(
-		precision,
-		&(vec->v[1 + base->nb_output_dims + orig_params + it * 2 + 1]),
-		1); // ubi
+		osl_relation_insert_vector(base, vec, -1);
+		osl_vector_free(vec);
 
-	osl_relation_insert_vector(base, vec, -1);
+		vec = osl_vector_pmalloc(precision, base->nb_columns);
 
-	osl_int_set_si(precision,
-				   &(vec->v[1 + base->nb_output_dims + orig_params + it * 2]),
-				   -1); // -lbi
-	osl_relation_insert_vector(base, vec, -1);
+		// ubi >= 0 && ubi -lbi >= 0
+		osl_int_set_si(precision, &(vec->v[0]), 1); // >= 0
+		osl_int_set_si(
+			precision,
+			&(vec->v[1 + base->nb_output_dims + orig_params + it * 2 + 1]),
+			1); // ubi
 
-	osl_vector_free(vec);
+		osl_relation_insert_vector(base, vec, -1);
 
-	vec = osl_vector_pmalloc(precision, base->nb_columns);
+		osl_int_set_si(
+			precision,
+			&(vec->v[1 + base->nb_output_dims + orig_params + it * 2]),
+			-1); // -lbi
+		osl_relation_insert_vector(base, vec, -1);
 
-	// -i + ubi >= 0
-	osl_int_set_si(precision, &(vec->v[0]), 1);		  // >= 0
-	osl_int_set_si(precision, &(vec->v[1 + it]), -1); // -i
-	osl_int_set_si(
-		precision,
-		&(vec->v[1 + base->nb_output_dims + orig_params + it * 2 + 1]),
-		1); // ubi
+		osl_vector_free(vec);
 
-	osl_relation_insert_vector(base, vec, -1);
-	osl_vector_free(vec);
+		vec = osl_vector_pmalloc(precision, base->nb_columns);
+
+		// -i + ubi >= 0
+		osl_int_set_si(precision, &(vec->v[0]), 1);		  // >= 0
+		osl_int_set_si(precision, &(vec->v[1 + it]), -1); // -i
+		osl_int_set_si(
+			precision,
+			&(vec->v[1 + base->nb_output_dims + orig_params + it * 2 + 1]),
+			1); // ubi
+
+		osl_relation_insert_vector(base, vec, -1);
+		osl_vector_free(vec);
+
+		base = base->next;
+	}
 }
 
 static void atiling_gen_update_domain(atiling_fragment_p frag,
@@ -328,7 +338,7 @@ static void atiling_gen_update_domain(atiling_fragment_p frag,
 
 	for (int i = 0; i < num_it; i++) {
 		if (is_tiling_enabled(frag, i)) {
-			atiling_gen_domain(stmt, stmt->domain, orig_params, i);
+			atiling_gen_domain(stmt, orig_params, i);
 		}
 	}
 }
@@ -364,9 +374,15 @@ static void atiling_gen_iinner_loop(FILE *output, atiling_fragment_p frag,
 	while (stmt != NULL) {
 		// Insert new cols
 		for (int i = 0; i < added_params; i++) {
+			osl_relation_p domain = stmt->domain;
 
-			osl_relation_insert_blank_column(stmt->domain,
-											 stmt->domain->nb_columns - 1);
+			while (domain != NULL) {
+				osl_relation_insert_blank_column(domain,
+												 domain->nb_columns - 1);
+				domain->nb_parameters++;
+
+				domain = domain->next;
+			}
 			osl_relation_insert_blank_column(stmt->scattering,
 											 stmt->scattering->nb_columns - 1);
 
@@ -377,7 +393,7 @@ static void atiling_gen_iinner_loop(FILE *output, atiling_fragment_p frag,
 				list = list->next;
 			}
 		}
-		stmt->domain->nb_parameters += added_params;
+
 		stmt->scattering->nb_parameters += added_params;
 
 		osl_relation_list_p list = stmt->access;
@@ -536,6 +552,9 @@ static void atiling_gen_iloop(FILE *output, atiling_fragment_p fragment,
 
 #define MAX_STR 512
 
+/**
+ * Write the trahrhe domain argument inside a string
+ */
 void atiling_sprint_trahrhe(char *s, atiling_fragment_p fragment) {
 	int cursor		= 0;
 	osl_scop_p scop = fragment->scop;
@@ -608,8 +627,15 @@ void gen_macros(FILE *output, char *out, int id) {
 	fprintf(output, "%s\n", ATILING_GEN_MAX);
 }
 
-void atiling_gen_header(atiling_fragment_p fragment,
-						atiling_options_p options) {
+/**
+ * Generate .trahrhe file
+ * This file is used by the trahrhe bash script
+ * It contains on the first line the trahrhe domain arg
+ * On the second line the name of the header used in #include
+ * On the third line there is the number of tiled level
+ */
+void atiling_gen_info_file(atiling_fragment_p fragment,
+						   atiling_options_p options) {
 	pid_t pid;
 	int ret;
 	ATILING_debug("Generating trahrhe header");
